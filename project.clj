0c4ea52b95db8299fe2874014dce829312310de4
Michiel Borkent <michielborkent@gmail.com>
2014-09-27 20:01:49 +0200

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberagent/lein-template "0.1.0"
+(defproject liberagent/lein-template "0.1.1"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
