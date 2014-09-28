4d88da7bd6a2e3dcc6d407141135cfbbe2ae4c9e
Michiel Borkent <michielborkent@gmail.com>
2014-09-28 17:24:57 +0200

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberagent/lein-template "0.1.3"
+(defproject liberagent/lein-template "0.1.4-SNAPSHOT"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
